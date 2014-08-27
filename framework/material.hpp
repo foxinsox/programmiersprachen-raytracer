87839017f31afba68be77fb83cc404bcd8bb1a75
foxinsox <development@alexanderpospischil.de>
2014-08-27 15:18:06 +0200

    material als DTO implementiert

--- framework/material.hpp
+++ framework/material.hpp
@@ -1,8 +1,19 @@
 #ifndef BUW_MATERIAL_HPP
 #define BUW_MATERIAL_HPP
 
-class Material{
+struct Material{
 
+
+Material()
+:ambient_color_(),diffuse_color_(),specular_color_(),specular_exp_(200.0f){}
+
+Material(Color const& ambient, Color const& diffuse, Color const& specular, float const specular_exponent)
+: ambient_color_(ambient), diffuse_color_(diffuse),specular_color_(specular), specular_exp_(specular_exponent){} 
+
+Color ambient_color_;
+Color diffuse_color_;
+Color specular_color_;
+float specular_exp_;
 
 };
 

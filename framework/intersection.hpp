2018f08269ea374aedf808a93cdd56e4aa329827
foxinsox <development@alexanderpospischil.de>
2014-08-27 10:45:43 +0200

    intersection.hpp angelegt.

---
+++ framework/intersection.hpp
@@ -0,0 +1,24 @@
+#ifndef BUW_INTERSECTION_HPP
+#define BUW_INTERSECTION_HPP
+
+#import "material.hpp"
+
+struct intersection{
+
+Intersection(glm::vec3 p, glm::vec3 n)
+: point(p),normal(n){}
+
+
+glm::vec3 point;
+glm::vec3 normal;
+
+//TODO: pointer auf material einfügen
+
+
+float t;
+bool hit;
+
+
+};
+
+#endif //BUW_INTERSECTION_HPP

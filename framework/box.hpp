107a5a1c8596652559fbe493801e2dd2111505c4
foxinsox <development@alexanderpospischil.de>
2014-08-27 16:09:53 +0200

    Standardbox header erstellt. individualisierung per transformationsmatritzen.
    This reverts commit e3ab5dadb95e7c228ab6c78bf12350d94eb45e82.

--- framework/box.hpp
+++ framework/box.hpp
@@ -7,9 +7,20 @@ class Box : public Shape{
 
 public:
 
-Box(std::shared_ptr<Material> const& material);
+Box(glm::vec3 const& pMinimum, glm::vec3 const& pMaximum)
+:pMin_(pMinimum),pMax_(pMaximum){}
+
+glm::vec3 const pMin();
+void pMin(glm::vec3 const& p);
+glm::vec3 const pMax();
+void pMax(glm::vec3 const& p);
+
 Intersection intersect_with(Ray const& r) const override;
-//einheitsbox wird angelegt und dann per scaling, translation und rotation individualisiert
+
+
+private:
+glm::vec3 pMin_;
+glm::vec3 pMax_;
 
 
 };

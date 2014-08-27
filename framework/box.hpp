88d64c81b1ea831b8a260a2114b60af1f156ce4d
foxinsox <development@alexanderpospischil.de>
2014-08-27 16:38:36 +0200

    header von sphere und box als standards beschrieben

--- framework/box.hpp
+++ framework/box.hpp
@@ -7,22 +7,9 @@ class Box : public Shape{
 
 public:
 
-Box(glm::vec3 const& pMinimum, glm::vec3 const& pMaximum)
-:pMin_(pMinimum),pMax_(pMaximum){}
-
-glm::vec3 const pMin();
-void pMin(glm::vec3 const& p);
-glm::vec3 const pMax();
-void pMax(glm::vec3 const& p);
+Box(std::shared_ptr<Material> const& material);
 
 Intersection intersect_with(Ray const& r) const override;
-
-
-private:
-glm::vec3 pMin_;
-glm::vec3 pMax_;
-
-
 };
 
 #endif //BUW_BOX_HPP
---
+++ framework/sphere.hpp
@@ -0,0 +1,15 @@
+#ifndef BUW_SPHERE_HPP
+#define BUW_SPHERE_HPP
+
+#include "intersection.hpp"
+
+class Sphere : public Shape{
+
+public:
+	Sphere(std::shared_ptr<Material> const& material);
+	Intersection intersect_with(Ray const& r) const override;
+
+	//center und radius werden einheitlich festgelegt und dann per scaling und translation angepasst.
+};
+
+#endif //BUW_SPHERE_HPP

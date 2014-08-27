6bbbb0889456311e6e4c58ebc1b4cdfb26c13ebb
foxinsox <development@alexanderpospischil.de>
2014-08-27 17:44:03 +0200

    constructor um scene erweitert

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -3,18 +3,20 @@
 // License    : MIT (see the file LICENSE)
 // Maintainer : Andreas-C. Bernstein <andreas.bernstein@uni-weimar.de>
 // Stability  : experimental
-//
+// Edited by  : Alexander Pospischil
 // Renderer
 // -----------------------------------------------------------------------------
 
 #include "renderer.hpp"
 
-Renderer::Renderer(unsigned w, unsigned h, std::string const& file)
+
+Renderer::Renderer(unsigned w, unsigned h, std::string const& file, Scene const& scene)
   : width_(w)
   , height_(h)
   , colorbuffer_(w*h, Color(0.0, 0.0, 0.0))
   , filename_(file)
   , ppm_(width_, height_)
+  , scene_(scene)
 {}
 
 void Renderer::render()
--- framework/renderer.hpp
+++ framework/renderer.hpp
@@ -3,13 +3,16 @@
 // License    : MIT (see the file LICENSE)
 // Maintainer : Andreas-C. Bernstein <andreas.bernstein@uni-weimar.de>
 // Stability  : experimental
-//
+// Edited by  : Alexander Pospischil
 // Renderer
 // -----------------------------------------------------------------------------
 
 #ifndef BUW_RENDERER_HPP
 #define BUW_RENDERER_HPP
 
+#include "scene.hpp"
+#include "intersection.hpp"
+#include "ray.hpp"
 #include "color.hpp"
 #include "pixel.hpp"
 #include "ppmwriter.hpp"
@@ -19,7 +22,8 @@
 class Renderer
 {
 public:
-  Renderer(unsigned w, unsigned h, std::string const& file);
+  Renderer(unsigned w, unsigned h, std::string const& file, Scene const& scene);
+
 
   void render();
   void write(Pixel const& p);
@@ -30,6 +34,12 @@ public:
   }
 
 private:
+
+  Intersection trace (Ray const& r) const;
+  Color shade(Ray const& r, Intersection const& isec) const;
+  Color shadow(Ray const& r) const;
+
+  Scene const& scene_;
   unsigned width_;
   unsigned height_;
   std::vector<Color> colorbuffer_;

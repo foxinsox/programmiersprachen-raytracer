4fcbf515850d0897ecb63adcb8579287da393a59
foxinsox <development@alexanderpospischil.de>
2014-08-27 17:43:16 +0200

    added transformation overrides

--- framework/composite.hpp
+++ framework/composite.hpp
@@ -9,6 +9,10 @@ public:
 	void add_child(std::shared_ptr<Shape> cld);
 	void remove_child(std::shared_ptr<Shape> cld);
 
+	void translate(glm::vec3 const& t) override;
+	void scale(glm::vec3 const& s) override;
+	void rotate(double const radians, glm::vec3 const& axis) override;
+
 	Intersection intersect_with(Ray const& r) const override;
 
 private:

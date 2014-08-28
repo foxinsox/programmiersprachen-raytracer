5c449a616fe0faf9868bf2ce009946482006ccee
foxinsox <development@alexanderpospischil.de>
2014-08-28 17:04:12 +0200

    erste pseudoszene

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -15,8 +15,17 @@ int main(int argc, char* argv[])
   Camera cam;
   auto mat = std::make_shared<Material>();
   auto sphere = std::make_shared<Sphere>(mat);
+  auto box = std::make_shared<Box>(mat);
+
+  glm::vec3 translationVector1(-1200,-1200,-10000);  //0,0,-10000 ohne scaling
+  glm::vec3 translationVector2(1000,1000,-10000);
+  sphere->translate(translationVector1);
+  sphere->scale(glm::vec3(0.2,0.2,0.2));      //0.2
+  box->translate(translationVector2);
+  box->scale(glm::vec3(0.2,0.2,0.1));
   scene.camera(cam);
   scene.add_shape(sphere);  
+  scene.add_shape(box);
 
 
 

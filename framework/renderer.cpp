82f3b94279e926789baf67bf703037e7f3d7c962
foxinsox <development@alexanderpospischil.de>
2014-08-28 17:03:52 +0200

    schwarzweiß primary-tracing

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -25,27 +25,24 @@ void Renderer::render()
 
   Camera cam = scene_.camera();
 
-  for (unsigned h = 0; h < height_; ++h) {
-    for (unsigned w = 0; w < width_; ++w) {
+  glm::vec2 res(width_, height_);
+
+  for (unsigned y = 0; y < height_; ++y) {
+    for (unsigned x = 0; x < width_; ++x) {
 
 //von screen(auflösungs)-Koordinaten in Kamera-Koordinaten umrechnen:
-      double image_aspect_ratio = width_/height_;
-      double alph = M_PI/4;  //TODO: automatisieren!
-      double pixelNormalizedX = (w+0.5)/width_;
-      double pixelNormalizedY = (h+0.5)/height_;
-      double pixelRemappedX = 2*pixelNormalizedX-1;
-      double pixelRemappedY = 1 - 2*pixelNormalizedY;
-      double pixelCameraX = (2*pixelRemappedX - 1)*image_aspect_ratio*tan(alph/2);
-      double pixelCameraY = (1 - 2*pixelRemappedY)*tan(alph/2);
-      glm::ivec2 screen_coord(pixelCameraX,pixelCameraY);
-      Pixel p(pixelCameraX,pixelCameraY);
+      glm::vec2 pos(x,y);
+      glm::vec2 sample_pos = pos / res;
+      
       int rayDepth = 1000; //TODO: automatisieren!
 
       //primary-rays erzeugen!
-      Ray ray = cam.generate_ray_at(screen_coord, rayDepth);
+      Ray ray = cam.generate_ray_at(sample_pos, rayDepth);
 
       //primary tracing:
       Intersection i = trace(ray);
+
+      Pixel p(x, y);
       
       //first test:
       if(i.hit == true){

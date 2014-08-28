3797ca7cd6cd8dd2965d961b3ee5174e25f084b6
foxinsox <development@alexanderpospischil.de>
2014-08-28 12:15:21 +0200

    funktion calculate_transformationMatrix eingefuegt

--- framework/camera.hpp
+++ framework/camera.hpp
@@ -8,19 +8,17 @@ class Camera{
 
 public:
 	Camera();
-	Camera(glm::vec3 const& eye_, glm::vec3 const& direction_, glm::vec3 const& up_, float fov_);
-	Ray generate_ray_at(glm::ivec2 const& screen_coord, int depth)const;
+	Camera(glm::vec3 const& eye_, glm::vec3 const& direction_, glm::vec3 const& up_, float fov_degrees);
+	Ray generate_ray_at(glm::ivec2 const& screen_coord, int rayDepth)const;
 
 	void translate(glm::vec3 const& translationVector);
 	void rotate(float degrees, glm::vec3 const& axis);
 
 
 private:
+	void calculate_transformationMatrix(glm::vec3 const& eye_, glm::vec3 const& direction_, glm::vec3 const& up_);
 	glm::mat4 camera_to_world_;
 	glm::mat4 world_to_camera_;
-	glm::vec3 eye;
-	glm::vec3 direction;
-	glm::vec3 up;
 	glm::vec2 fov;
 	float depth;  //focal length. muss berechnet werden (im konstruktor?)
 };

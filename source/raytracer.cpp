54a2ebbdd25ba8a55ccc9f05f8065b25122a725c
foxinsox <development@alexanderpospischil.de>
2014-08-31 18:24:04 +0200

    render-dauer ausgabe

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <chrono>
 #include <renderer.hpp>
 #include <fensterchen.hpp>
 #include "sdf_reader.hpp"
@@ -6,13 +7,20 @@
 int main(int argc, char* argv[])
 {
 
+bool use_window = true;
+
 std::string SDF_filepath;
 if(argc>=2){
-SDF_filepath = argv[1];
+  SDF_filepath = argv[1];
+  if (argc>=3 && strcmp(argv[2], "--no-window") == 0) {
+    use_window = false;
+  } else {
+    std::cout << "ERROR: unknown flag '" << argv[2] << "'" << std::endl;
+  }
 }
 else{
-std::cout<<"ERROR: no File to be opened"<<std::endl;
-return 0;
+  std::cout<<"ERROR: no File to be opened"<<std::endl;
+  return 0;
 }
 
   SDFReader reader(SDF_filepath);
@@ -23,23 +31,30 @@ return 0;
   std::shared_ptr<Renderer> app = reader.renderer();
 
   std::thread thr([&app]() {
+    auto begin = std::chrono::high_resolution_clock::now();
     app->render(); 
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
+    std::cout << "rendered frame in " << duration << "ms" << std::endl;
   });
 
-  Window win(app->dimension());
-  
+  if (use_window) {
+
+    Window win(app->dimension());
+    
 
 
 
-  while (!win.shouldClose()) {
-    if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
-      win.stop();
+    while (!win.shouldClose()) {
+      if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
+        win.stop();
+      }
+
+      glDrawPixels( app->dimension().x, app->dimension().y, GL_RGB, GL_FLOAT
+          , app->colorbuffer().data());
+
+      win.update();
     }
-
-    glDrawPixels( app->dimension().x, app->dimension().y, GL_RGB, GL_FLOAT
-        , app->colorbuffer().data());
-
-    win.update();
   }
 
   thr.join();
